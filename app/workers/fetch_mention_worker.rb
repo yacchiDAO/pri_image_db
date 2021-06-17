be30c2a32109038a70e0d1107c73e56bb907bf87
yacchiDAO <okamura@net.lab.uec.ac.jp>
2021-06-18 01:21:07 +0900

    リプライのロジック修正2

--- app/workers/fetch_mention_worker.rb
+++ app/workers/fetch_mention_worker.rb
@@ -5,17 +5,34 @@ class FetchMentionWorker
   def perform
     return unless Rails.env.production?
 
-    service = Twitter::TweetService.new
     current = Time.current
     service.twitter.mentions_timeline.each do |tw|
       # 自身のツイートは無視
-      next if tw.user.screen_name == "PriImage"
-      next if tw.retweet?
+      next if tw.user.screen_name == "PriImage" || tw.retweet?
+      next if distance_seconds(current, tw.id.to_i) > 20.0
 
-      # https://qiita.com/riocampos/items/e5544325211976f2cfc1
-      distance_seconds = current - Time.at(((tw.id.to_i >> 22) + 1_288_834_974_657) / 1000.0)
+      # ループ内になるけど毎回叩くよりアクセス数が減る見込み
+      last_id = Rails.cache.read(redis_key).to_i
+      # 同一のリプライ済みツイートがあるため=も弾く
+      next if last_id >= tw.id.to_i
 
-      service.search_reply(tw) if distance_seconds <= 10.0
+      service.search_reply(tw)
+      Rails.cache.write(redis_key, tw.id.to_i)
     end
   end
+
+  private
+
+    def redis_key
+      "last_reply"
+    end
+
+    def service
+      @service ||= Twitter::TweetService.new
+    end
+
+    def distance_seconds(current, id)
+      # https://qiita.com/riocampos/items/e5544325211976f2cfc1
+      current - Time.at(((id >> 22) + 1_288_834_974_657) / 1000.0)
+    end
 end
