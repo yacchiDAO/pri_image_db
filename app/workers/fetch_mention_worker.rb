6ed6ee7240cf9017df2db5499115d4565857a918
yacchiDAO <okamura@net.lab.uec.ac.jp>
2021-06-17 18:05:25 +0900

    有効なリプの計算方法修正

--- app/workers/fetch_mention_worker.rb
+++ app/workers/fetch_mention_worker.rb
@@ -6,13 +6,14 @@ class FetchMentionWorker
     return unless Rails.env.production?
 
     service = Twitter::TweetService.new
+    current = Time.current
     service.twitter.mentions_timeline.each do |tw|
       # 自身のツイートは無視
       next if tw.user.screen_name == "PriImage"
       next if tw.retweet?
 
       # https://qiita.com/riocampos/items/e5544325211976f2cfc1
-      distance_seconds = Time.current - Time.at(((tw.id.to_i >> 22) + 1_288_834_974_657) / 1000.0)
+      distance_seconds = current - Time.at(((tw.id.to_i >> 22) + 1_288_834_974_657) / 1000.0)
 
       service.search_reply(tw) if distance_seconds <= 10.0
     end
