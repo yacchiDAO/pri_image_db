bd3c4f193457e6ec0a36365f99444f346db6b331
yacchiDAO <okamura@net.lab.uec.ac.jp>
2024-10-18 00:24:17 +0900

    sidekiq redis verify

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = if Rails.env.production?
-                   { url: ENV.fetch("REDIS_URL") }
+                   { url: ENV.fetch("REDIS_URL"), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
                  else
                    { url: "redis://redis:6379", namespace: "sidekiq" }
                  end
@@ -8,7 +8,7 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = if Rails.env.production?
-                   { url: ENV.fetch("REDIS_URL") }
+                   { url: ENV.fetch("REDIS_URL"), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
                  else
                    { url: "redis://redis:6379", namespace: "sidekiq" }
                  end
